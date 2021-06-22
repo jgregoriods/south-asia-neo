2040bb101132d4d6ece108d723d7981bf9748a8f
jgregoriods <jonas.gregorio@gmail.com>
2021-06-22 16:35:11 +0200

    first commit

---
+++ R/poly_src.R
@@ -0,0 +1,173 @@
+library(gdistance)
+#library(rcarbon)
+library(parallel)
+
+ORIGIN <- c(42.45, 36.37)
+START <- 11748
+DATES <- read.csv("sites/dates.csv")
+coordinates(DATES) <- ~Longitude+Latitude
+proj4string(DATES) <- CRS("+init=epsg:4326")
+
+#cal <- calibrate(DATES$C14, DATES$SD, verbose=FALSE)
+#DATES$bp <- medCal(cal)
+
+ELE_RAW <- raster("layers/ele.tif")
+PREC_RAW <- raster("layers/prec.tif")
+
+
+normRaster <- function(x) {
+    return ((x - min(values(x), na.rm=TRUE)) / (max(values(x), na.rm=TRUE) - min(values(x), na.rm=TRUE)))
+}
+
+
+# Transform and scale
+ELE <- normRaster((ELE_RAW)^(1/3))
+
+# Remove outliers and 0 (for log transform)
+maxVal <- quantile(PREC_RAW, .99)
+PREC_RAW[values(PREC_RAW > maxVal)] <- maxVal
+PREC_RAW[values(PREC_RAW) <= 0] <- 0.1
+
+PREC <- normRaster(log(PREC_RAW))
+PREC <- abs(PREC - 1)
+
+
+simulateDispersal <- function(costRaster, origin, date) {
+    tr <- transition(costRaster, function(x) 1 / mean(x), 16)
+    tr <- geoCorrection(tr)
+    ac <- accCost(tr, origin)
+    ac <- ac / 1000
+    ac[values(ac) == Inf] <- NA
+    simDates <- date - ac
+    return(simDates)
+}
+
+
+compareDates <- function(simRaster, dates) {
+    dates$simbp <- extract(simRaster, dates)
+    dates$dist <- spDistsN1(dates, ORIGIN, longlat=TRUE)
+    plot(dates$dist, dates$bp)
+    points(dates$dist, dates$simbp, col="red")
+}
+
+
+testModel <- function(costRaster, sites=DATES, origin=ORIGIN, date=START) {
+    simDates <- simulateDispersal(costRaster, origin, date)
+    gc() 
+    # Score
+    sites$simbp <- extract(simDates, sites)
+    sites <- sites[!is.na(sites$simbp),]
+    rmse <- sqrt(sum((sites$simbp - sites$bp)^2) / nrow(sites))
+    return(rmse)
+}
+
+
+qtl <- function(x, newVals=1:4) {
+    vals <- quantile(x, na.rm=T)
+    m <- c(vals[1], vals[2], newVals[1],
+           vals[2], vals[3], newVals[2],
+           vals[3], vals[4], newVals[3],
+           vals[4], vals[5], newVals[4])
+    rclmat <- matrix(m, ncol=3, byrow=T)
+    rc <- reclassify(x, rclmat)
+    rc[values(rc) == vals[1]] <- newVals[1]
+    return(rc)
+}
+
+
+getCost <- function(x, coefs) {
+    a <- coefs[1]
+    b <- coefs[2]
+    c <- coefs[3]
+    return(abs((a*(x^2)) + (b*x) + c))
+}
+
+
+# Genetic Algorithm
+
+crossover <- function(x, y) {
+    i <- sample(1:(length(x)-1), 1)
+    return(c(x[1:i], y[(i+1):length(y)]))
+}
+
+mutate <- function(x) {
+    i <- sample(1:length(x), 1)
+    x[i] <- x[i] + rnorm(1)
+    return(x)
+}
+
+numGenomes <- 100
+numParents <- 20
+numElite <- 5
+mutationRate <- 0.2
+numIter <- 20
+
+# Initialize genomes
+genomes <- as.data.frame(matrix(nrow=numGenomes, ncol=7))
+for (i in 1:numGenomes) {
+    genomes[i,] <- c(rnorm(6, mean=1, sd=3), Inf)
+}
+
+maxScores <- c()
+avgScores <- c()
+
+cat("Running genetic algorithm. This may take a while...\n")
+pb <- txtProgressBar(min=0, max=numIter, style=3)
+setTxtProgressBar(pb, 0)
+for (iter in 1:numIter) {
+
+    for (i in 1:numGenomes) {
+        if (genomes[i,7] == Inf) {
+            costEle <- getCost(ELE, as.numeric(genomes[i,][1:3]))
+            costPrc <- getCost(PREC, as.numeric(genomes[i,][4:6]))
+            genomes[i,7] <- testModel(costEle+costPrc)
+            gc()
+        }
+    }
+    
+    avgScores[iter] <- mean(genomes[,7])
+    
+    elite <- genomes[order(genomes[,7]),][1:numElite,]
+    parents <- genomes[order(genomes[,7]),][1:numParents,]
+    parents <- parents[order(as.numeric(rownames(parents))),]
+
+    maxScores[iter] <- elite[1,7]
+
+    children <- as.data.frame(matrix(nrow=numGenomes - numElite, ncol=7))
+    j <- 1
+    while (j <= numGenomes - numElite) {
+        for (i in seq(1, numParents, 2)) {
+            if (j > numGenomes - numElite) {
+                break
+            }
+            parent1 <- as.numeric(parents[i,])[1:6]
+            parent2 <- as.numeric(parents[i+1,])[1:6]
+            child <- crossover(parent1, parent2)
+            if (runif(1) < mutationRate) {
+                child <- mutate(child)
+            }
+            children[j,] <- c(child, Inf)
+            j <- j+1
+        }
+    }
+    genomes <- rbind(elite, children)
+    rownames(genomes) <- sample(1:numGenomes)
+    setTxtProgressBar(pb, iter)
+}
+close(pb)
+
+#plot(avgScores, type="l", col="blue")
+#lines(maxScores, col="red")
+
+best <- as.numeric(genomes[1,])
+costEle <- getCost(ELE, as.numeric(best[1:3]))
+costPrc <- getCost(PREC, as.numeric(best[4:6]))
+cost <- costEle+costPrc
+simDates <- simulateDispersal(costEle+costPrc, ORIGIN, START)
+#plot(simDates)
+
+#compareDates(simDates, DATES)
+
+write.csv(avgScores, "avgScores.csv")
+write.csv(maxScores, "maxScores.csv")
+write.csv(genomes, "genomes.csv")
