6d881190ec07b9a3f6f49fdac5b20b6b24f6232d
jgregoriods <jonas.gregorio@gmail.com>
2022-03-10 13:57:35 +0100

    cleanup

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>South Asia Neolithic Expansion</h1>
+
+Jonas Gregorio de Souza<br/>
+[![ORCiD](https://img.shields.io/badge/ORCiD-0000--0001--7879--4531-green.svg)](https://orcid.org/0000-0001-6032-4443)<br/>
+
+This repository contains the radiocarbon dates and code for simulating the rate of diffusion of the agricultural package from the Near East to the Indian subcontinent across different environments.
+
+To be published as part of
+
+de Souza, J.G.; Ruiz-Pérez, J.; Lancelotti, C.; Madella, M. (ms) <i>Environmental and geographical effects on the spread of the Neolithic crop package to South Asia.</i>
+
+Different costs of traversing each type of terrain are assigned and the optimal values are searched through a genetic algorithm, with the fitness score given by comparison with the radiocarbon dates.
---
+++ main.R
@@ -0,0 +1,64 @@
+source("src/src.R")
+
+# Parameters for the genetic algorithm. We will be using 11 terrain classes,
+# a population of 500, 250 parents to be transmitted to the next generation,
+# 50 elite (preserved without mutation), 2% chance of mutation and 20 generations.
+numGenes <- 10
+numGenomes <- 500
+numParents <- 250
+numElite <- 50
+mutationRate <- 0.2
+numIter <- 20
+
+
+# Testing the sites of Mureybet and Dhra as potential origins
+for (originSite in c("Mureybet", "Dhra")) {
+    # See src for documentation of genetic algorithm
+    res <- GA(numGenes, numGenomes, numParents, numElite, mutationRate, numIter, originSite, cores=10)
+
+    # Get the values from the best model and generate a friction surface
+    best <- as.numeric(res$genomes[1,])
+    reclassMatrix <- cbind(1:numGenes, best[1:numGenes])
+    costRaster <- reclassify(BIOMES, reclassMatrix)
+    costSPDF <- as(costRaster, "SpatialPixelsDataFrame")
+    names(costSPDF@data) <- c("val")
+
+    # Load cost surface in GRASS with a resolution of 25 km. See src for
+    # documentation of the GRASS interface functions.
+    setGRASS(costSPDF, 25000)
+
+    # Calculate the speeds (inverse of the cost)
+    speedRaster <- 1 / costRaster
+    speedRaster.wgs <- projectRaster(speedRaster, res=0.25, crs=WGS, method="ngb")
+    speedRaster.cropped <- crop(speedRaster.wgs, extent(DATES))
+
+    # Coordinates and start yr BP of the chosen origin site
+    ORIGIN <- as.numeric(DATES.m[DATES.m$Site==originSite,]@coords)
+    START <- DATES.m[DATES.m$Site==originSite,]$bp
+
+    # Simulate arrival times. See src for function documentation
+    simDates <- simulateDispersal(ORIGIN, START)
+    simDates.wgs <- projectRaster(simDates, res=0.25, crs=WGS)
+    simDates.r <- crop(simDates.wgs, extent(DATES))
+
+    cat(paste("Best score:", best[numGenes+1], "\n"))
+
+    # Write files
+    # Rasters used in QGIS to produce the maps in the paper
+    speeds <- data.frame("region"=1:numGenes, "speed"=1/best[1:numGenes])
+    writeRaster(speedRaster.cropped, paste("results/speed_", originSite, ".tif", sep=""), overwrite=T)
+    write.csv(speeds, file=paste("results/res_", originSite, ".csv", sep=""))
+    save(res, simDates, file=paste("results/ga_", originSite, ".RData", sep=""))
+    writeRaster(simDates.r, paste("results/sim_", originSite, ".tif", sep=""), overwrite=T)
+
+    # Fig 3 and S1 Fig
+    print(plotDates(simDates, DATES.m, ORIGIN, best[11]))
+    try(dev.print(tiff, paste("results/plot_", originSite, ".tiff", sep=""), res=300, width=1280))
+    try(dev.off())
+
+    # S3-S4 Fig
+    par(bg="white")
+    plotGA(res, paste("Origin:", originSite))
+    try(dev.print(tiff, paste("results/scores_", originSite, ".tiff", sep=""), res=300, width=2000))
+    try(dev.off())
+}
